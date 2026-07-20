f2063695a79dd0d35ed4eaa24d3aee37d57f8685
Dhanushganesan13 <dhanushganesan10@gmail.com>
2026-07-20 23:54:01 +0530

    Create Count uppercase and lowercase letters.c

---
+++ Strings/Count uppercase and lowercase letters.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i, upper = 0, lower = 0;
+    gets(str);
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+            upper++;
+        else if(str[i] >= 'a' && str[i] <= 'z')
+            lower++;
+    }
+    printf("Uppercase = %d\n", upper);
+    printf("Lowercase = %d\n", lower);
+    return 0;
+}
