7999caf22732d3e5bb07c9229c0f8cc2e247d0f2
Dhanushganesan13 <dhanushganesan10@gmail.com>
2026-07-20 19:50:16 +0530

    Create Count the number of words.c

---
+++ Strings/Count the number of words.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i, words=1;
+    gets(str);
+    for(i=0; str[i]!='\0'; i++)
+    {
+        if(str[i]==' ')
+            words++;
+    }
+    printf("Number of Words = %d", words);
+    return 0;
+}
