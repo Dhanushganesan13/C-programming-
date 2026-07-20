7e0d48431d65b27d68f9d5dd76053a244a18d0ba
Dhanushganesan13 <dhanushganesan10@gmail.com>
2026-07-20 18:43:03 +0530

    Create Count number of digits.c

---
+++ Strings/Count number of digits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i, count = 0;
+    gets(str);
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= '0' && str[i] <= '9')
+            count++;
+    }
+    printf("Number of Digits = %d", count);
+    return 0;
+}
